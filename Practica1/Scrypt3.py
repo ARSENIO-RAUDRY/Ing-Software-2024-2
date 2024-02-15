562451f969f933aad1335b026ccdb0e36dcceefb
Arsenio <arsenio@ciencias.unam.mx>
2024-02-14 23:27:17 -0600

    Agrega Scrypt3 con la funciona graficar

---
+++ Practica1/Scrypt3.py
@@ -0,0 +1,9 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-8,8,1)
+y1 = abs(x)/x * (.3 *abs(x) + .2 *(abs(abs(x)-1)) + 2.2*(abs(abs(x)-2)) - 2.7*(abs(abs(x)-3)) - 3*(abs(abs(x)-5)) + 3*(abs(abs(x)-7)) + 5*np.sin( (math.pi/4) * (abs(abs(x)-3) - abs(abs(x)-4) +1 )) + (5/4)*(abs(abs(x)-4) - abs(abs(x)-5) -1)**3 - 5.3*np.cos( ((math.pi/2) + np.arcsin(47/53)) * ((abs(abs(x)-7)) - abs((abs(x)-8))-1)/2) +2.8)
+
+plt.plot(x,y1)
+plt.show()
