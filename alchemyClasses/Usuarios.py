92c87783ef41f15afe4aad501eb001d6abaf74f4
peter clemenza <arsenio@ciencias.unam.mx>
2024-02-22 14:11:17 -0600

    Corrige usuarios, empieza a codear en peliculas

--- alchemyClasses/Usuarios.py
+++ alchemyClasses/Usuarios.py
@@ -3,15 +3,15 @@ from sqlalchemy import Column, Integer, String, LargeBinary
 from alchemyClasses import db
 
 class Usuarios(db.Model):
-    __tablename__ = 'usuario'
+    __tablename__ = 'usuarios'
     id_usuario = Column(Integer, primary_key = True)
     nombre = Column(String(200))
     apellido_pat = Column(String(200))
     apellido_mat = Column(String(200))
     password = Column(String(64))
     email = Column(String(500))
-    profile_picture = Column(LargeBinary())
-    superuser= Column(Int(1))
+    profile_picture = Column(LargeBinary)
+    superuser= Column(Integer)
     
     def __init__(self, nombre, apellido_pat, apellido_mat, password, email, profile_picture=None, superuser):
         self.nombre = nombre
