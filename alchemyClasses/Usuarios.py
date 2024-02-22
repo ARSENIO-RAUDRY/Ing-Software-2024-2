1c5c1102ba6c2076978f6f5051a5a445fd6faa6b
peter clemenza <arsenio@ciencias.unam.mx>
2024-02-22 13:54:58 -0600

    Codigo listo de la clase Usuarios

--- alchemyClasses/Usuarios.py
+++ alchemyClasses/Usuarios.py
@@ -13,5 +13,14 @@ class Usuarios(db.Model):
     profile_picture = Column(LargeBinary())
     superuser= Column(Int(1))
     
-def __str__(self):
-    return f'Id Usuario{self.id_usuario}\n Nombre:{self.nombre}\n Apellido Paterno:{self.apellido}'
+    def __init__(self, nombre, apellido_pat, apellido_mat, password, email, profile_picture=None, superuser):
+        self.nombre = nombre
+        self.apellido_pat = apellido_pat
+        self.apellido_mat = apellido_mat
+        self.password = password
+        self.email = email
+        self.profile_picture = profile_picture
+        self.superuser = superuser
+    
+    def __str__(self):
+        return f'Id Usuario: {self.id_usuario}\nNombre:{self.nombre}\nApellido Paterno:{self.apellido_pat}\nApellido Materno:{self.apellido_mat}\nCorreo:{self.email}'
