061c5cd1b6f1786fa255c01139f06fd4c7d19291
Arsenio <arsenio@ciencias.unam.mx>
2024-02-24 14:18:50 -0600

    Crea funcion nueva renta

--- model/model_rentar.py
+++ model/model_rentar.py
@@ -9,7 +9,41 @@ def get_renta(id_renta):
     return Rentar.query.filter(Rentar.id_renta == id_renta).first()
 
 def genera_nueva_fecha():
-    anio
+    anio = 0
+
+    while(True):
+        try:
+            anio = int(input("Ingresa el nuevo año:"))
+            if anio <1969 or anio >2024:
+                raise ValueError
+            else: break
+
+        except ValueError:
+            print("Ingresa una fecha valida")
+
+    mes=0
+    while(True):
+        try:
+            mes = int(input("Ingresa el nuevo mes:"))
+            if mes <1 or mes>12:
+                raise ValueError
+            else: break
+
+        except ValueError:
+            print("Ingresa una fecha valida")
+
+    dia = 0
+    while(True):
+        try:
+            dia = int(input("Ingresa el nuevo dia:"))
+            if dia < 1 or (dia > 28 and mes == 2) or (dia > 31 and mes != 2):
+                raise ValueError
+            else: break
+
+        except ValueError:
+            print("Ingresa una fecha valida")
+
+    return datetime.date(year=anio, month=mes, day=dia)
 
 def cambia_fecha(id_renta, nueva_fecha):
     renta = get_renta(id_renta)
